ce94171a17acb9d7c0bb070952a445ffbe58b5c6
Bas van der Hoek <bas@hoek.com>
2025-05-07 14:24:41 +0200

    Add alias for clippy and coverage

--- shell.nix
+++ shell.nix
@@ -6,4 +6,9 @@ mkShell {
     sonar-scanner-cli
     libiconv-darwin
   ];
+  shellHook = ''
+    alias sonars="RUSTFLAGS=-Aclippy::missing_safety_doc \
+      cargo clippy --message-format=json > target/clippy.json; \
+      cargo tarpaulin --skip-clean --no-fail-fast --output-dir target --out Lcov"
+  '';
 }
